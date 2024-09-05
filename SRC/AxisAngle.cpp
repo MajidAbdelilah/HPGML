0aee8a496d71194ceab7cd199b4316843fd2c057
amajid <abdelilahmajid012@gmail.com>
2024-09-05 23:21:11 +0100

    fixing a typo

--- SRC/AxisAngle.cpp
+++ SRC/AxisAngle.cpp
@@ -1,11 +1,13 @@
 #include "AxisAngle.h"
 
+#define toRadians(x) x * 0.01745329251f
 
 //axis need to be a normalized vector
-Vector3 RotateVectorAroundAxisAngle(const Vector3& v, const Vector3& axis, float angle)
+Vector3 RotateVectorAroundAxisAngle(const Vector3& v, const Vector3& axis, const float &angle)
 {
-    float sinAngle = sin(angle);
-    float cosAngle = cos(angle);
+    float a = toRadians(angle);
+    float sinAngle = sin(a);
+    float cosAngle = cos(a);
 
-    return v * cosAngle + axis.Cross(v) * sinAngle + axis * axis.DotProduct(v) * (1 - cosAngle);
+    return v * cosAngle + axis.Cross(v) * sinAngle + axis * v.DotProduct(axis) * (1 - cosAngle);
 }
